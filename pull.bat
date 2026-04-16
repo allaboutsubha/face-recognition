9c4757a91e03868ce97f899e760b529a7d28c266
allaboutsubha <subhakarmakar443@gmail.com>
2026-04-16 19:07:27 +0530

    a

--- pull.bat
+++ pull.bat
@@ -1,6 +1,15 @@
 @echo off
+:: প্রথমে চেক করবে এই ফোল্ডারে গিট সেটআপ আছে কি না, না থাকলে সেট করবে
+if not exist .git (
+    echo Initializing Git and setting remote...
+    git init
+    git remote add origin https://github.com/allaboutsubha/face-recognition.git
+)
+
+echo Pulling latest code from GitHub...
 git pull origin main
+
 echo.
-echo Updated! Closing in 3 seconds...
-timeout /t 3 >nul
+echo Updated! Closing in 5 seconds...
+timeout /t 5 >nul
 exit
